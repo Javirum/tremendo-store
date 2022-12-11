93b3e2f830577538bd578932355a5775a92676e4
Javi <romeropaolini@gmail.com>
2022-12-11 10:26:58 +0100

    Create User schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema'
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -21,7 +22,7 @@ export default config({
         url: databaseURL,
     },
     lists: createSchema({
-
+        User
     }),
     ui: {
         isAccessAllowed: () => true,
