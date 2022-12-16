e2c1050d79741a1a8b6707214996313541667cf4
Javi <romeropaolini@gmail.com>
2022-12-16 14:37:22 +0100

    Fix nav bar aligment issue and update colors

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,36 +2,35 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
 
-const Logo = styled.h1 `
-    background: var(--green, 30 215 96);
-    margin-left: 2rem;
-    font-size: 2rem;
-    position: relative;
-    z-index: 2; 
-    transform: skew(-7deg);
-    a {
-        color: white;
-        text-decoration: none;
-        text-transform: uppercase;
-        padding: 0.5rem 1rem;
-    }
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: var(--green, green);
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
 `;
 
-const HeaderStyles = styled.header `
-    .bar {
-        border-bottom: 10px solid var(--black, black);
-        display: grid;
-        grid-template-columns: auto 1fr:
-        justify-content: space-between;
-        align-items: stretch;
-    }
-
-    .sub-bar {
-        display: grid;
-        grid-template-columns: auto 1fr:
-        border-bottom: 10px solid var(--black, black);
-    }
-`
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header() {
     return (
